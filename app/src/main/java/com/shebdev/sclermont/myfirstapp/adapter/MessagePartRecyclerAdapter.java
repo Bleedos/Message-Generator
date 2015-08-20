493e45c5bc3628535e479e19adeb97473ea1fef1
Sébastien Clermont <sebastien.clermont@desjardins.com>
2015-08-19 20:30:11 -0400

    Recycler view un peu plus fonctionnel avec differentiation du click sur le texte ou sur l'image

--- app/src/main/java/com/shebdev/sclermont/myfirstapp/adapter/MessagePartRecyclerAdapter.java
+++ app/src/main/java/com/shebdev/sclermont/myfirstapp/adapter/MessagePartRecyclerAdapter.java
@@ -1,10 +1,10 @@
 package com.shebdev.sclermont.myfirstapp.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,26 +23,35 @@ public class MessagePartRecyclerAdapter extends RecyclerView.Adapter<MessagePart
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data item is just a string in this case
         public TextView mTextView;
+        public ImageView mImageView;
         public IMyViewHolderClicks mListener;
 
-        public ViewHolder(TextView v, IMyViewHolderClicks listener) {
-            super(v);
-            mTextView = v;
+        public ViewHolder(View itemLayoutView, IMyViewHolderClicks listener) {
+            super(itemLayoutView);
+            mTextView = (TextView) itemLayoutView.findViewById(R.id.recycler_view_text);
+            mImageView = (ImageView) itemLayoutView.findViewById(R.id.recycler_view_image);
             mListener = listener;
-            v.setOnClickListener(this);
+            mImageView.setOnClickListener(this);
+            mTextView.setOnClickListener(this);
 
         }
 
         @Override
         public void onClick(View v) {
-
+            Toast.makeText(v.getContext(),
+                    "click "+v.getClass().getCanonicalName(),
+                    Toast.LENGTH_LONG).show();
             if (v instanceof TextView) {
                 mListener.onPotato((TextView)v, getPosition());
+            }
+            else if (v instanceof ImageView) {
+                mListener.onTomato((ImageView)v, getPosition());
             }
         }
 
         public static interface IMyViewHolderClicks {
             public void onPotato(TextView caller, int position);
+            public void onTomato(ImageView caller, int position);
         }
 
     }
@@ -58,14 +67,18 @@ public class MessagePartRecyclerAdapter extends RecyclerView.Adapter<MessagePart
                                                    int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.my_text_view, parent, false);
+                .inflate(R.layout.message_part_recycler_view, parent, false);
         // set the view's size, margins, paddings and layout parameters
-        ViewHolder vh = new ViewHolder((TextView)v, new MessagePartRecyclerAdapter.ViewHolder.IMyViewHolderClicks() {
+        ViewHolder vh = new ViewHolder(v, new MessagePartRecyclerAdapter.ViewHolder.IMyViewHolderClicks() {
             public void onPotato(TextView caller, int position) {
             // TODO: Onclick ici
                 String texttt = caller.getText().toString();
                 caller.setAlpha(0.6f);
-                caller.append("bob"+position+"::"+mDataset[position]);
+                caller.append("bob" + position + "::" + mDataset[position]);
+            };
+            public void onTomato(ImageView caller, int position) {
+                // TODO: Onclick ici
+                caller.setAlpha(0.6f);
             }
         });
         return vh;
@@ -77,7 +90,7 @@ public class MessagePartRecyclerAdapter extends RecyclerView.Adapter<MessagePart
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.mTextView.setText(mDataset[position]);
-
+        holder.mImageView.setImageResource(R.drawable.ic_delete_white_24dp);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
